5d65bfbac753ef846f9c66318537c77e8638ac48
sielzz <93588547+sielzz@users.noreply.github.com>
2024-12-05 20:40:40 +0700

    config.py

--- config.py
+++ config.py
@@ -7,22 +7,22 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = API_ID
-API_HASH = "API_HASH"
+API_ID = 22712175
+API_HASH = "514deebe1216bc7325c2d5768eff6334"
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = "BOT_TOKEN"
+BOT_TOKEN = "5098762797:AAFsC9xdsvEt-MauUG_Gx0M7NGkay6vTK_M"
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = "MONGO_DB_URI"
+MONGO_DB_URI = "mongodb+srv://arsilaf:030403april@cluster0.6rjsa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
 # Chat id of a group for logging bot's activities
-LOG_GROUP_ID = LOG_GROUP_ID
+LOG_GROUP_ID = -1001742877853
 
 # Get this value from @ultron2_robot on Telegram by /id
-OWNER_ID = OWNER_ID
+OWNER_ID = 2032022524
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -32,23 +32,23 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/rishabhops/alice",
+    "https://github.com/sielzz/alice",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = "https://t.me/thanos_pro"
-SUPPORT_GROUP = "https://t.me/thanosprosss"
+SUPPORT_CHANNEL = "https://t.me/aaf_store"
+SUPPORT_GROUP = "https://t.me/publikwihh"
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 
 
 # Get this credentials from https://developer.spotify.com/dashboard
-SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
-SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
+SPOTIFY_CLIENT_ID = getenv("6Lfv7YMqAAAAAPiAqqqo0nLwt660SKTPKiW7neJO")
+SPOTIFY_CLIENT_SECRET = getenv("6Lfv7YMqAAAAANZOJGUg7aqmOoNs9FQxF09IlmDC")
 
 
 # Maximum limit for fetching playlist's track from youtube, spotify, apple links.
@@ -62,7 +62,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
 
 
 # Get your pyrogram v2 session from Replit
-STRING1 = "STRING_SESSION"
+STRING1 = "BQB-toWaJOykoi49AHeYEpynhcBnI41gDZpQkqIgdjDi8UI7hThslVgNZpqeITL8ACiuz7jLX6mvfiyPq2UVQNgJiTaULV7crOyH7a7iAudPqKph7c8cBxunCFgC12jzSi4JQs0NrInxYoNvhlDyHEAPRhHdrfviSoHU_vf4EouYF-cowcrjsIW8HekTJc52jyEmeCphwMpnBk93gUMJgbMprSw69UqaFJ0HCNGn4ghUcGk5-yGachfKjbcf8Qe65eEND4rMF0uZlo4ynNJ47PiiNJsBi7KVeQ0BjEaEGT6uN0oiGya5BfIQGv4LQOfCh6qIJFVVDgelEYU65bNisrf5AAAAAdgouZYA"
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
