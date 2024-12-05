975afada62ff688c08bc1ef345b07af510aefd89
sielzz <93588547+sielzz@users.noreply.github.com>
2024-12-05 21:29:37 +0700

    userbot.py

--- alicex/core/userbot.py
+++ alicex/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("thanosprosss")
-                await self.one.join_chat("learning_bots")
+                await self.one.join_chat("aaf_store")
+                await self.one.join_chat("publikwihh")
             except:
                 pass
             assistants.append(1)
